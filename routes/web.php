32fafd7500af6df1e9f9e1d35fc99b1ce60db4e9
Yusup Ridwan <joseph.rizvan@gmail.com>
2024-11-02 09:45:12 +0700

    commit models, controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
